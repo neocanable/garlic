03779eafa0eeb51bce71ec077a6428362ab954e6
Kreijstal <rainb@tfwno.gf>
2025-06-03 20:27:31 +0000

    fix build for linux

--- src/common/endian.h
+++ src/common/endian.h
@@ -8,26 +8,36 @@
  * */
 
 #ifndef __APPLE__
-#warning "This header file (endian.h) is MacOS X specific.\n"
-#endif	/* __APPLE__ */
-
-#ifdef __APPLE__
+#include <endian.h>
+#include <stdint.h>
+#include <byteswap.h>
+#ifndef be16toh
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define be16toh(x) __bswap_16(x)
+#else
+#define be16toh(x) (x)
+#endif
+#endif
+#ifndef be32toh
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+#define be32toh(x) __bswap_32(x)
+#else
+#define be32toh(x) (x)
+#endif
+#endif
+#else
 #include <libkern/OSByteOrder.h>
-
 #define htobe16(x) OSSwapHostToBigInt16(x)
 #define htole16(x) OSSwapHostToLittleInt16(x)
 #define be16toh(x) OSSwapBigToHostInt16(x)
 #define le16toh(x) OSSwapLittleToHostInt16(x)
-
 #define htobe32(x) OSSwapHostToBigInt32(x)
 #define htole32(x) OSSwapHostToLittleInt32(x)
 #define be32toh(x) OSSwapBigToHostInt32(x)
 #define le32toh(x) OSSwapLittleToHostInt32(x)
-
 #define htobe64(x) OSSwapHostToBigInt64(x)
 #define htole64(x) OSSwapHostToLittleInt64(x)
 #define be64toh(x) OSSwapBigToHostInt64(x)
 #define le64toh(x) OSSwapLittleToHostInt64(x)
-
 #endif
 #endif	/* __FINK_ENDIANDEV_PKG_ENDIAN_H__ */
